a976fae2733ac2f57d09315d888b675068c1d2a2
Jasmine <hellooworld.warner@gmail.com>
2023-05-01 00:21:52 +1000

    Added relative line numbers to nvim-tree

--- lua/jasmine/nvim-cmp.lua
+++ lua/jasmine/nvim-cmp.lua
@@ -7,96 +7,93 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
--- empty setup using defaults
-require("nvim-tree").setup()
-
-
-
 -- OR setup with some options
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+    sort_by = "case_sensitive",
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = true,
+    },
 })
+
+require 'nvim-tree.view'.View.winopts.relativenumber = true
 
 local cmp = require('cmp')
 local luasnip = require('luasnip')
 
 local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
 end
 
 local select_prev_item = function(fallback)
-  if cmp.visible() then
-    cmp.select_next_item()
-  elseif luasnip.expand_or_jumpable() then
-    luasnip.expand_or_jump()
-  elseif has_words_before() then
-    cmp.complete()
-  else
-    fallback()
-  end
+    if cmp.visible() then
+        cmp.select_next_item()
+    elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+    elseif has_words_before() then
+        cmp.complete()
+    else
+        fallback()
+    end
 end
 
 local select_next_item = function(fallback)
-  if cmp.visible() then
-    cmp.select_prev_item()
-  elseif luasnip.jumpable(-1) then
-    luasnip.jump(-1)
-  else
-    fallback()
-  end
+    if cmp.visible() then
+        cmp.select_prev_item()
+    elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+    else
+        fallback()
+    end
 end
 
 cmp.setup({
-  mapping = {
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<C-e>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
+    mapping = {
+        ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+        ['<C-e>'] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+        }),
+        ['<C-Space>'] = cmp.mapping(
+            cmp.mapping.confirm({
+                behavior = cmp.ConfirmBehavior.Insert,
+                select = true,
+            }),
+            { 'i', 'c' }
+        ),
+        ['<Tab>'] = cmp.mapping(select_prev_item, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(select_next_item, { 'i', 's' }),
+        ['<Down>'] = cmp.mapping(select_prev_item, { 'i', 's' }),
+        ['<Up>'] = cmp.mapping(select_next_item, { 'i', 's' }),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+    },
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+        { name = 'path' },
+        { name = 'luasnip' },
     }),
-    ['<C-Space>'] = cmp.mapping(
-      cmp.mapping.confirm({
-        behavior = cmp.ConfirmBehavior.Insert,
-        select = true,
-      }),
-      { 'i', 'c' }
-    ),
-    ['<Tab>'] = cmp.mapping(select_prev_item, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(select_next_item, { 'i', 's' }),
-    ['<Down>'] = cmp.mapping(select_prev_item, { 'i', 's' }),
-    ['<Up>'] = cmp.mapping(select_next_item, { 'i', 's' }),
-    ['<CR>'] = cmp.mapping.confirm({ select = false }),
-  },
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'buffer' },
-    { name = 'path' },
-    { name = 'luasnip' },
-  }),
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end,
+    },
 })
 
 cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'cmdline' },
-  },
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'cmdline' },
+    },
 })
 
 cmp.setup.cmdline('/', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' },
-  },
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' },
+    },
 })
