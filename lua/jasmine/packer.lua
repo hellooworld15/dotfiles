c8e8e610631a4a88bfa1dcdd91b153125214c31b
Jasmine <hellooworld.warner@gmail.com>
2023-05-01 09:12:02 +1000

    Adding vertical line indents

--- lua/jasmine/packer.lua
+++ lua/jasmine/packer.lua
@@ -2,88 +2,97 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim',
-                    'nvim-tree/nvim-web-devicons'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim',
+            'nvim-tree/nvim-web-devicons' } }
+    }
 
     use({
-      "folke/trouble.nvim",
-      config = function()
-          require("trouble").setup {
-              icons = false,
-              -- your configuration comes here
-              -- or leave it empty to use the default settings
-              -- refer to the configuration section below
-          }
-      end
-  })
+        "folke/trouble.nvim",
+        config = function()
+            require("trouble").setup {
+                icons = false,
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    })
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('nvim-treesitter/playground')
 
-  -- Quick movement between files 
-  use('ThePrimeagen/harpoon')
+    -- Quick movement between files
+    use('ThePrimeagen/harpoon')
 
-  -- keep information about file changes
-  use('mbbill/undotree')
+    -- keep information about file changes
+    use('mbbill/undotree')
 
-  -- Pope pugins
-  use('tpope/vim-fugitive')
-  use("nvim-treesitter/nvim-treesitter-context");
+    -- Pope pugins
+    use('tpope/vim-fugitive')
+    use("nvim-treesitter/nvim-treesitter-context");
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v1.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},             -- Required
-		  {'williamboman/mason.nvim'},           -- Optional
-		  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},         -- Required
-		  {'hrsh7th/cmp-nvim-lsp'},     -- Required
-		  {'hrsh7th/cmp-buffer'},       -- Optional
-		  {'hrsh7th/cmp-path'},         -- Optional
-		  {'saadparwaiz1/cmp_luasnip'}, -- Optional
-		  {'hrsh7th/cmp-nvim-lua'},     -- Optional
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },         -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+            { 'hrsh7th/cmp-buffer' },       -- Optional
+            { 'hrsh7th/cmp-path' },         -- Optional
+            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' },     -- Optional
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},             -- Required
-		  {'rafamadriz/friendly-snippets'}, -- Optional
-	  }
-  }
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },             -- Required
+            { 'rafamadriz/friendly-snippets' }, -- Optional
+        }
+    }
 
-  use("folke/zen-mode.nvim")
-  use("github/copilot.vim")
+    use("folke/zen-mode.nvim")
+    use("github/copilot.vim")
 
-  -- This is absolutely purely for my pleasure
-  use("eandrju/cellular-automaton.nvim")
-  use("laytan/cloak.nvim")
+    -- This is absolutely purely for my pleasure
+    use("eandrju/cellular-automaton.nvim")
+    use("laytan/cloak.nvim")
 
-  use {
-      'nvim-tree/nvim-tree.lua',
-      requires = {
-          'nvim-tree/nvim-web-devicons', -- optional
-      },
-  }
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional
+        },
+    }
 
-  -- colour manager
-  use 'RRethy/nvim-base16'
-  use 'kyazdani42/nvim-palenight.lua'
+    -- colour manager
+    use 'RRethy/nvim-base16'
+    use 'kyazdani42/nvim-palenight.lua'
 
-  -- Git signs for edits made
-use {
-  'lewis6991/gitsigns.nvim',
-  config = function()
-    require('gitsigns').setup()
-  end
-}
+    -- Adding autopairs
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
+
+    -- indent line: alas I can see again - horizontal lines drive me insane though 0/10 reccomend
+    use "lukas-reineke/indent-blankline.nvim"
+
+    -- Git signs for edits made
+    use {
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require('gitsigns').setup()
+        end
+    }
 end)
